793d15c8a366a8a0aa5aa0cf6afa9e6fa6ea00de
zeyan-ansari <zeyanansari10@gmail.com>
2026-05-03 18:35:54 +0530

    fixes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const http = require('http');
+
 const port = process.env.PORT || 3000;
+const env = process.env.APP_ENV || "not-set";
+
 http.createServer((req, res) => {
-  res.end('Hello Zeyan 🚀 Your app is LIVE!');
+  res.end(`Hello Sir 🚀\nEnvironment: ${env}`);
 }).listen(port);
+
+console.log("Server running...");
