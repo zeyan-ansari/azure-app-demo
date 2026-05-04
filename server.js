7bf6c9c5a6ff085e7aa9fdddc4238c891f989e5e
zeyan-ansari <zeyanansari10@gmail.com>
2026-05-04 13:24:01 +0530

    setup ci cd

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const env = process.env.APP_ENV || "not-set";
 
 http.createServer((req, res) => {
-  res.end(`Hello Sir 🚀\nEnvironment: ${env}`);
+  res.end(`Hello Zeyan 🚀\n app is live: ${env}`);
 }).listen(port);
 
 console.log("Server running...");
